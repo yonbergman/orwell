7d91bc23ae5be5fc2aa914cb3d5ff027f2756a81
yon <Lighthawky@gmail.com>
2014-01-20 01:01:53 +0200

    added actualy face detection

--- app/actors/face_detection.rb
+++ app/actors/face_detection.rb
@@ -5,6 +5,7 @@ class FaceDetection
 
   def initialize(capture)
   	@capture = capture
+  	@detector = ::OpenCV::CvHaarClassifierCascade::load("#{ENV['OPENCV_DIR']}/haarcascades/haarcascade_frontalface_alt.xml")
   	async.run
   end
 
@@ -14,6 +15,9 @@ class FaceDetection
 			require "opencv" unless defined? ::OpenCV
 			@capture = ::OpenCV::CvCapture::open unless @capture
 			frame = @capture.query 
+			@detector.detect_objects(frame).each do |rect|
+   				frame.rectangle! rect.top_left, rect.bottom_right, :color => ::OpenCV::CvColor::Red
+   			end
 			frame.save('tmpframe.png')
 		end
 	rescue Exception => e
