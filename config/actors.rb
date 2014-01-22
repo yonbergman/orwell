908c24642b0579cb3a840447f26d119e53a3e22c
yon <Lighthawky@gmail.com>
2014-01-22 17:41:03 +0200

    fallback for not being able to start face detection

--- config/actors.rb
+++ config/actors.rb
@@ -2,5 +2,9 @@ require 'celluloid/autostart'
 require 'opencv'
 
 door = DoorOpener.new ENV['DOOR_ADDR']
+begin
 face = FaceDetection.new ::OpenCV::CvCapture::open
+rescue Exception
+  puts "Couldn't start face detection"
+end
 sound_player = SoundPlayer.new
